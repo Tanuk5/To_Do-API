d14f468d16afebcd7fe6b9071062fea39025c56e
tanu kumari <tanukumarisinha.123@gmail.com>
2022-09-06 13:02:49 +0530

    create_

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-console.log("hello tanu");
-console.log("how are you");
-
 const express = require('express');
 const dotenv = require("dotenv");
 const todoRoutes = require('./routes/todo');
@@ -8,7 +5,6 @@ const todoRoutes = require('./routes/todo');
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
-
 
 app.use(express.json());  //we can also use body-parser
 app.use('./todo', todoRoutes);
--- routes/todo.js
+++ routes/todo.js
@@ -1,23 +1,37 @@
 const router = require('express').Router();
-const Todo = require('./models/todo');
+const Todo = require('../models/todo');
 
 //List of all todd
 router.get('/', (req,res)=> {
-
+    // Todo.find().exec((err, todos) =>{
+    //     if(err){
+    //         return res.json({ error: err});
+    //     }
+    //     return res.json({ data: todos});
+    // });
 });
 
 //Create of all todo
 router.post('/', (req,res)=> {
-
+//      const todo = Todo({
+//         title: req.body.title,
+//         content: req.body.content,
+//      });
+//      todo.save((err, todo) => {
+//         if(err){
+//             return res.json({ error: err});
+//         }
+//         return res.json({ data: todo});
+//      });
 });
 
 //Edit of all todo
-router.put('/', (req,res)=> {
+router.put('/:id', (req,res)=> {
 
 });
 
 //Delete of all todo
-router.delete('/', (req,res)=> {
+router.delete('/:id', (req,res)=> {
 
 });
 
