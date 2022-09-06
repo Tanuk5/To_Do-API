a67457e8ac1da6a11b99e8b7550205dc7a557f09
tanu kumari <tanukumarisinha.123@gmail.com>
2022-09-06 09:34:03 +0530

    started_port

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // console.log("hello tanu");
+// console.log("how are you");
+// console.log("I know you are not fine buddy");
 const express = require('express');
 const app = express();
+const PORT = 8880;
 
 app.use(express.json());  //we can also use body-parser
 
-app 
+app.get('/', (req,res)=> res.json({'message':'your server is running '}));
+app.listen(PORT, () => console.log (`server started at ${PORT}`));
+
